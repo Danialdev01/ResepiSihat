fd8168c1ea6e4152aa631cb5ab2cfd68672b2b09
Danial Irfan <developersdani@gmail.com>
2025-09-15 14:56:33 +0200

    Update head.php

--- components/head.php
+++ components/head.php
@@ -13,7 +13,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo $location_index?>/src/assets/css/output.css">
     <link rel="stylesheet" href="<?php echo $location_index?>/node_modules/flowbite/dist/flowbite.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
+    <!-- <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" /> -->
     <link rel="shortcut icon" href="<?php echo $location_index?>/src/assets/images/logo/favicon/favicon.ico" type="image/x-icon">
 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
